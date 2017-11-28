1e089771b80a02b0d0570a7610c329f5ebdc1450
root <root@stretch.localdomain>
2017-11-28 23:19:42 +0000

    redefined role and profile eddited

--- site/profile/manifests/databases.pp
+++ site/profile/manifests/databases.pp
@@ -1,9 +1,7 @@
 class profile::databases {
   class { '::mysql::server':
-#    ensure =>absent,
     root_password           => 'strongpassword',
     remove_default_accounts => true,
-#    override_options        => $override_options
   }
 }
 
